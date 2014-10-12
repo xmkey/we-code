9d56322c04b85e24fb89913f0a31e5ecf71fbb15
jeakey <june_fh@qq.com>
2014-10-12 13:35:14 +0800

    数据库

---
+++ we-code.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.6
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2014 年 10 月 09 日 14:48
+-- 服务器版本: 5.1.44
+-- PHP 版本: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `we2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `match`
+--
+
+CREATE TABLE IF NOT EXISTS `match` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(200) DEFAULT NULL,
+  `from` bigint(20) DEFAULT NULL,
+  `to` bigint(20) DEFAULT NULL,
+  `timer` int(11) DEFAULT NULL,
+  `ismatch` tinyint(1) DEFAULT NULL,
+  `num` bigint(20) DEFAULT NULL,
+  `time` int(11) DEFAULT NULL,
+  `code` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=107 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
