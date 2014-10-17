3779756109ea2867356aad8d8ed3dff1b7b15222
xmagicwu <xmagicwu@gmail.com>
2014-10-17 14:10:04 +0800

    联调
    联调

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,22 +1,22 @@
 <?php
-return array(
-	'DB_TYPE'   => 'mysql', // 数据库类型
-	'DB_HOST'   => 'localhost', // 服务器地址
-	'DB_NAME'   => 'we2', // 数据库名
-	'DB_USER'   => 'root', // 用户名
-	'DB_PWD'    => '', // 密码
-	'DB_PORT'   => 3306, // 端口
-	'DB_PREFIX' => '', // 数据库表前缀 
-	'DB_CHARSET'=> 'utf8', // 字符集
-);
 // return array(
 // 	'DB_TYPE'   => 'mysql', // 数据库类型
-// 	'DB_HOST'   => 'rdsnrujrznrujrz.mysql.rds.aliyuncs.com', // 服务器地址
-// 	'DB_NAME'   => 'we-code', // 数据库名
-// 	'DB_USER'   => 'wecode', // 用户名
-// 	'DB_PWD'    => 'wecode', // 密码
+// 	'DB_HOST'   => 'localhost', // 服务器地址
+// 	'DB_NAME'   => 'we2', // 数据库名
+// 	'DB_USER'   => 'root', // 用户名
+// 	'DB_PWD'    => '', // 密码
 // 	'DB_PORT'   => 3306, // 端口
 // 	'DB_PREFIX' => '', // 数据库表前缀 
 // 	'DB_CHARSET'=> 'utf8', // 字符集
-
 // );
+return array(
+	'DB_TYPE'   => 'mysql', // 数据库类型
+	'DB_HOST'   => 'rdsnrujrznrujrz.mysql.rds.aliyuncs.com', // 服务器地址
+	'DB_NAME'   => 'we-code', // 数据库名
+	'DB_USER'   => 'wecode', // 用户名
+	'DB_PWD'    => 'wecode', // 密码
+	'DB_PORT'   => 3306, // 端口
+	'DB_PREFIX' => '', // 数据库表前缀 
+	'DB_CHARSET'=> 'utf8', // 字符集
+
+);
